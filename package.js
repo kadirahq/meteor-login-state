e303e77bbe72801f56ffc114d23cc20f1ab316cc
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-22 21:17:22 +0530

    v1.3.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   "summary": "Share Meteor Login State Between Sub Domains",
-  "version": "1.3.0",
+  "version": "1.3.1",
   "git": "https://github.com/kadirahq/meteor-login-state.git",
   "name": "kadira:login-state"
 });
